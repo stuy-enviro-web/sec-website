98ec4af10b9996ad434bd5aa3ed9fa3888b0d3ce
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-06-09 18:38:04 -0400

    finish scaling main page basic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,10 +32,10 @@ export default function Home() {
             </div>
         </div>
         <UpcomingEvents />
-        <div className="w-full h-10"/>
+        <div className="w-full h-10" />
         <div className="w-full h-[65em] md:h-[45em] flex flex-col md:flex-none md:grid grid-cols-12 grid-rows-12 ">
             <h1 className="row-start-1 col-start-1 col-span-full text-7xl text-center miltonian mt-3">About Us</h1>
-            <Image src={image_medley} className="visible md:invisible row-start-2 mx-auto w-full h-auto p-2" alt="Medley of images" />
+            <Image src={image_medley} className="max-h-[45em] visible md:invisible row-start-2 mx-auto w-full h-auto p-2" alt="Medley of images" />
             <p className="row-start-3 col-start-4 col-end-10 row-span-full text-md font-light lexend text-1xl p-3">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
@@ -49,6 +49,50 @@ export default function Home() {
             <Image src={hard_at_work} className="invisible md:visible col-start-1 col-end-4 row-start-2 row-end-6 w-full h-auto p-2" alt="A picture of the SEC members hard at work" />
             <Image src={club_board} className="invisible md:visible w-full h-auto col-start-1 col-end-4 row-start-8 row-span-full p-2" alt="A picture of the SEC club board" />
             <Image src={funny_faces} className="invisible md:visible w-full h-auto row-start-2 row-end-6 col-start-10 col-span-full p-2" alt="A picture of the SEC members making funny faces" />
+        </div>
+        <div className="w-full h-[60em] grid grid-cols-12 grid-rows-12">
+            <h1 className="row-start-1 text-5xl miltonian col-start-2 col-span-full">Helpful Links</h1>
+            <p className="row-start-2 col-start-2 col-end-11 text-md font-light lexend">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <div className="row-start-6 md:row-start-4 col-start-2 col-span-full flex flex-col items-start justify-start text-md font-light lexend">
+                <h3 className=" text-2xl font-bold miltonian">
+                    Old Website
+                </h3>
+
+                <ul className="text-md font-light lexend list-disc pl-5">
+                    <li><a className="hover:underline" href="https://stuyenviro.weebly.com/">SEC&apos;s Old Website</a></li>
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+
+                </ul>
+                <br/>
+                <h3 className="text-2xl font-bold miltonian">
+                    Socials
+                </h3>
+                <ul className="text-md font-light lexend list-disc pl-5">
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+
+                </ul>
+                <br />
+                <h3 className="text-2xl font-bold miltonian">
+                    Other
+                </h3>
+                <ul className="text-md font-light lexend list-disc pl-5">
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+                    <li><a className="hover:underline" href="https://google.com">LOREM IPSUM</a></li>
+
+                </ul>
+
+            </div>
+
         </div>
     </div>
     );
