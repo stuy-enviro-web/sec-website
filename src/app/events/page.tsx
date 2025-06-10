24ab3661db43454b2e82266eaefd0490244189da
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-06-10 12:47:54 -0400

    imaging updates

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -11,39 +11,41 @@ import ArrowRight from "public/arrow_right.svg";
 
 export default function events() {
     return (<div>
-        <div className="grid grid-cols-12 grid-rows-12 w-full h-[45em] overflow-y-hidden">
-            <h1 className="miltonian col-start-2 row-start-4 col-span-full text-left text-8xl">
+        <div className="relative grid grid-cols-12 grid-rows-12 w-full h-[45em] overflow-y-hidden">
+            <h1 className="miltonian col-start-2 row-start-4 col-span-full text-left text-8xl text-white z-10">
                 EVENTS!
             </h1>
 
             <a href="#upcoming" className="hover:underline group col-start-5 row-start-6 col-span-full items-center flex flex-row justify-left z-10">
-                <h3 className="lexend text-5xl group-hover:translate-x-1">
+                <h3 className="lexend text-4xl md:text-5xl group-hover:translate-x-1 text-white">
                     Upcoming Events
                 </h3>
-                <Image src={ArrowRight} className=" group-hover:translate-x-1 -translate-x-1 w-10 h-10 inline ml-2" alt="Arrow pointing right" />
+                <Image src={ArrowRight} className=" invert group-hover:translate-x-1 -translate-x-1 w-10 h-10 inline ml-2" alt="Arrow pointing right" />
             </a>
 
             <a href="#opportunities" className="hover:underline group col-start-5 row-start-7 col-span-full items-center flex flex-row justify-left z-10">
-                <h3 className="lexend text-5xl group-hover:translate-x-1">
+                <h3 className="lexend text-5xl group-hover:translate-x-1 text-white">
                     Opportunities
                 </h3>
-                <Image src={ArrowRight} className=" group-hover:translate-x-1 -translate-x-1 w-10 h-10 inline ml-2" alt="Arrow pointing right" />
+                <Image src={ArrowRight} className=" invert group-hover:translate-x-1 -translate-x-1 w-10 h-10 inline ml-2" alt="Arrow pointing right" />
             </a>
 
             <a href="#past" className="hover:underline group col-start-5 row-start-8 col-span-full items-center flex flex-row justify-left z-10">
-                <h3 className="lexend text-5xl group-hover:translate-x-1">
+                <h3 className="lexend text-5xl group-hover:translate-x-1 text-white">
                     Past Events
                 </h3>
-                <Image src={ArrowRight} className=" group-hover:translate-x-1 -translate-x-1 w-10 h-10 inline ml-2" alt="Arrow pointing right" />
+                <Image src={ArrowRight} className=" invert group-hover:translate-x-1 -translate-x-1 w-10 h-10 inline ml-2" alt="Arrow pointing right" />
             </a>
 
             <a href="#resources" className="hover:underline group col-start-5 row-start-9 col-span-full items-center flex flex-row justify-left z-10">
-                <h3 className="lexend text-5xl group-hover:translate-x-1">
+                <h3 className="lexend text-5xl group-hover:translate-x-1 text-white">
                     Resources
                 </h3>
-                <Image src={ArrowRight} className=" group-hover:translate-x-1 -translate-x-1 w-10 h-10 inline ml-2" alt="Arrow pointing right" />
+                <Image src={ArrowRight} className=" invert group-hover:translate-x-1 -translate-x-1 w-10 h-10 inline ml-2" alt="Arrow pointing right" />
             </a>
-            <Image src={bottleCapMural} className="row-start-1 row-span-full object-cover h-full w-auto lg:w-full lg:h-auto col-start-3 col-span-full -z-1 opacity-90" alt="The bottle cap mural made in 2016 at stuy that tells students to reduce, reuse, and recycle" />
+            {/* Dark overlay */}
+            <div className="absolute inset-0 bg-black opacity-60 z-0 pointer-events-none" />
+            <Image src={onTheWall} className="absolute bg-black mix-blend-darken row-start-1 row-span-full object-cover h-full w-auto lg:w-full lg:h-auto col-start-1 col-span-full -z-10 inset-0" alt="The bottle cap mural made in 2016 at stuy that tells students to reduce, reuse, and recycle" />
         </div>
         <div className="w-full h-10" />
         <div className="w-full h-[55em] grid grid-cols-12 grid-rows-12 ">
@@ -51,7 +53,7 @@ export default function events() {
                 Upcoming Events
             </h1>
 
-            <Image src={onTheWall} className="col-start-6 row-start-3 overflow-hidden p-3 col-span-full row-end-11 object-cover w-full h-auto" alt="The school board that SEC uses, which displays a chinese lunar new year thing in this image along with some other stuff" />
+            <Image src={bottleCapMural} className="col-start-6 row-start-3 overflow-hidden p-3 col-span-full row-end-11 object-cover w-full h-auto" alt="The school board that SEC uses, which displays a chinese lunar new year thing in this image along with some other stuff" />
         </div>
 
         <div className="w-full h-10" />
--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+import food_img from "public/media/hydroponics_2024.jpeg";
 
-
-export default function resources() {
-    return <div>
-        resources page
-    </div>
+export default function Resources() {
+    return (<div>
+        <div className="grid grid-cols-12 grid-rows-12 w-full h-[45em] overflow-y-hidden">
+            <h1 className="miltonian col-start-2 row-start-4 col-span-full text-left text-8xl">
+                Resources
+            </h1>
+            <Image src={food_img} className="row-start-1 row-span-full object-cover h-full w-auto lg:w-full lg:h-auto col-start-1 col-span-full -z-1 opacity-90" alt="Hydroponics system at Stuyvesant High School" />
+        </div>
+    </div>);
 }
