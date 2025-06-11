acc0b599dbe55954043713024842ecbfb7387470
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2025-06-10 23:39:05 -0400

    add sheets actions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,7 @@ import su_logo from "public/logos/SU_logo.svg"
 import sec_footer_logo from "public/logos/Env_Footer_Logo.png"
 import Image from "next/image"
 
+import FooterLeaders from "./FooterLeaders"
 
 
 export default function Footer() {
@@ -26,17 +27,7 @@ export default function Footer() {
         </div>
         <div className="row-start-4 sm:row-start-5 text-center text-sm col-start-3 sm:col-start-5 col-end-11 sm:col-end-9 flex flex-col">
             <br />
-            <div className="flex justify-between">
-                <span className="italic text-xs">President</span>
-                <span className="font-bold text-xs">Tralero Tralala</span>
-            </div>
-            <div className="flex justify-between">
-                <span className="italic text-xs">Vice President</span>
-                <span className="font-bold text-xs">Bombadilo Cracadilo</span>
-            </div>            <div className="flex justify-between">
-                <span className="italic text-xs">Secretary</span>
-                <span className="font-bold text-xs">Cappuchino Assassaino</span>
-            </div>
+            <FooterLeaders />
         </div>
         <div className="row-start-7 sm:row-start-8 text-center text-xs sm:text-sm col-start-1 col-span-full ">
             This Website was created by Elias Xu `25 and is maintained by the Stuyvesant Environmental Club. <br />
---
+++ src/components/FooterLeaders.tsx
@@ -0,0 +1,26 @@
+"use server";
+import { getSheetsData } from "@/actions/sheetsActions"
+
+export default async function FooterLeaders() {
+
+    const data = await getSheetsData(
+        process.env.FOOTER_LEADERS_SHEET_ID as string, "A1:B3")
+
+    console.log(data);
+
+    return (
+        <div>
+            <div className="flex justify-between">
+                <span className="italic text-xs">{data[0][0]}</span>
+                <span className="font-bold text-xs">{data[0][1]}</span>
+            </div>
+            <div className="flex justify-between">
+                <span className="italic text-xs">{data[1][0]}</span>
+                <span className="font-bold text-xs">{data[1][1]}</span>
+            </div>            <div className="flex justify-between">
+                <span className="italic text-xs">{data[2][0]}</span>
+                <span className="font-bold text-xs">{data[2][1]}</span>
+            </div>
+        </div>
+    )
+}
